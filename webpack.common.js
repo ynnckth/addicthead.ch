2761dbea0b43fd7752e328d762286f4255da2a17
ynnckth <yannickstreit@hotmail.com>
2018-02-08 18:40:55 +0100

    Setup project

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry: './client/src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'client/dist')
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({template: 'client/index.html'}),
+        new CleanWebpackPlugin(['client/dist'])
+    ]
+};
