6ef5e6780d952df28834225d1392b7b4df723eeb
ynnckth <yannickstreit@hotmail.com>
2018-02-09 23:37:52 +0100

    Fix header

--- client/src/components/header.component.js
+++ client/src/components/header.component.js
@@ -1,6 +1,7 @@
 
 import React from "react";
 import {Component} from "react";
+import {Link} from "react-router-dom";
 import banner from '../images/banner.png';
 import '../styles/header.component.css';
 
@@ -9,7 +10,9 @@ export default class Header extends Component {
 
     render() {
         return (
-            <img src={banner} alt="banner" />
+            <Link to={`/`}>
+                <img src={banner} alt="banner" />
+            </Link>
         )
     }
 }
--- client/src/pages/music.page.js
+++ client/src/pages/music.page.js
@@ -11,17 +11,17 @@ export default class Music extends Component {
             <div>
                 <h1>music</h1>
                 <div className="albums">
-                    <div class="album">
+                    <div className="album">
                         <iframe className="bandcamp-player"
                                 src="https://bandcamp.com/EmbeddedPlayer/album=547180559/size=large/bgcol=ffffff/linkcol=333333/transparent=true/"
                                 seamless><a href="http://addicthead.bandcamp.com/album/addicthead">Addicthead by ADDICTHEAD</a></iframe>
                     </div>
-                    <div class="album">
+                    <div className="album">
                         <iframe className="bandcamp-player"
                                 src="http://bandcamp.com/EmbeddedPlayer/album=2214690287/size=large/bgcol=ffffff/linkcol=333333/transparent=true/"
                                 seamless><a href="http://addicthead.bandcamp.com/album/strangers-7">Strangers 7&#39;&#39; by ADDICTHEAD</a></iframe>
                     </div>
-                    <div class="album">
+                    <div className="album">
                         <iframe className="bandcamp-player"
                                 src="http://bandcamp.com/EmbeddedPlayer/album=3305211210/size=large/bgcol=ffffff/linkcol=333333/transparent=true/"
                                 seamless><a href="http://addicthead.bandcamp.com/album/spring-sessions">Spring Sessions by ADDICTHEAD</a></iframe>
