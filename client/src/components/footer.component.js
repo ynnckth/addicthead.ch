4bfd033eeff72a28854100b568127867296af018
ynnckth <yannickstreit@hotmail.com>
2018-02-09 18:28:44 +0100

    Add footer

--- client/src/components/footer.component.js
+++ client/src/components/footer.component.js
@@ -1,13 +1,19 @@
 
 import React from "react";
 import {Component} from "react";
+import arrowUp from '../images/arrow-up-icon.png';
+import '../styles/footer.component.css';
 
 
 export default class Footer extends Component {
 
     render() {
         return (
-            <div>The footer goes here!</div>
+            <div className="footer">
+                <p>© 2018 by addicthead
+                    <a href="#"><img className="scroll-up-icon" src={arrowUp} /></a>
+                </p>
+            </div>
         )
     }
 }
--- client/src/components/header.component.js
+++ client/src/components/header.component.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import {Component} from "react";
-import banner from '../images/banner.png'
+import banner from '../images/banner.png';
 import '../styles/header.component.css';
 
 
