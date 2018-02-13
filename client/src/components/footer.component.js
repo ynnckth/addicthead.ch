e140bbdc42241ced6daea4c29901a9975166aceb
ynnckth <yannickstreit@hotmail.com>
2018-02-13 20:34:57 +0100

    Fix scroll to top from footer

--- client/src/components/footer.component.js
+++ client/src/components/footer.component.js
@@ -6,11 +6,18 @@ import '../assets/styles/footer.component.css';
 
 export default class Footer extends Component {
 
+    scrollToTop() {
+        document.body.scrollTop = 0;            // Safari
+        document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
+    }
+
     render() {
         return (
             <div className="footer">
                 <p>© 2018 by addicthead
-                    <a className="scroll-up-icon" href="#"><i className="fas fa-caret-up" /></a>
+                    <span onClick={this.scrollToTop} className="scroll-up-icon">
+                        <i className="fas fa-caret-up" />
+                    </span>
                 </p>
             </div>
         )
