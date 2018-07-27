b7ad260c8556348d90a4ab3fca25620984964089
ynnckth <yannickstreit@hotmail.com>
2018-07-27 20:15:53 +0200

    Fix embedded bandcamp players to be loaded over https

--- client/src/pages/music.page.js
+++ client/src/pages/music.page.js
@@ -18,12 +18,12 @@ export default class Music extends Component {
                     </div>
                     <div className="album">
                         <iframe className="bandcamp-player"
-                                src="http://bandcamp.com/EmbeddedPlayer/album=2214690287/size=large/bgcol=ffffff/linkcol=333333/transparent=true/"
+                                src="https://bandcamp.com/EmbeddedPlayer/album=2214690287/size=large/bgcol=ffffff/linkcol=333333/transparent=true/"
                                 seamless><a href="http://addicthead.bandcamp.com/album/strangers-7">Strangers 7&#39;&#39; by ADDICTHEAD</a></iframe>
                     </div>
                     <div className="album">
                         <iframe className="bandcamp-player"
-                                src="http://bandcamp.com/EmbeddedPlayer/album=3305211210/size=large/bgcol=ffffff/linkcol=333333/transparent=true/"
+                                src="https://bandcamp.com/EmbeddedPlayer/album=3305211210/size=large/bgcol=ffffff/linkcol=333333/transparent=true/"
                                 seamless><a href="http://addicthead.bandcamp.com/album/spring-sessions">Spring Sessions by ADDICTHEAD</a></iframe>
                     </div>
                 </div>
